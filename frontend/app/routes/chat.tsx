93c1de282424d5fdfe54cfe49aeda43bc2452726
Joshua Gartmeier <joshua@gartmeier.dev>
2025-04-22 22:49:57 +0200

    feat: add chat panel

--- frontend/app/routes/chat.tsx
+++ frontend/app/routes/chat.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/home";
-import { PenBox } from "lucide-react";
+import type { Route } from "./+types/chat";
+import { ArrowUp, PenBox } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,7 +10,16 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Chat() {
   return (
-    <div className="navbar">
+    <>
+      <Header />
+      <Panel />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header className="navbar">
       <div className="flex-1">
         <button
           className="btn btn-square btn-ghost"
@@ -26,6 +35,26 @@ export default function Chat() {
       <div className="flex-none flex gap-4">
         <button className="btn btn-primary">Sign In</button>
         <button className="btn">Sign Up</button>
+      </div>
+    </header>
+  );
+}
+
+function Panel() {
+  return (
+    <div className="card mx-auto bg-base-200 shadown-sm border-b-0 max-w-3xl fixed bottom-12 right-0 left-0">
+      <div className="card-body p-3">
+        <textarea
+          className="w-full resize-none focus:outline-none text-lg"
+          placeholder="Ask anything"
+          autoFocus
+          rows={2}
+        ></textarea>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary btn-circle">
+            <ArrowUp size={20} strokeWidth={3} />
+          </button>
+        </div>
       </div>
     </div>
   );
