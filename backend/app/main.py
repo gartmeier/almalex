9addfd2501332bba9b4195d6717281933daddade
Joshua Gartmeier <joshua@gartmeier.dev>
2025-04-24 17:18:28 +0200

    feat(backend): add JWT support and nanoid utility

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
+import jwt
 from fastapi import FastAPI
+
+from app.conf import settings
+from app.utils import nanoid
+
+SECRET_KEY = settings.secret_key
+ALGORITHM = "HS256"
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/token")
+async def login_for_access_token():
+    return jwt.encode({"sub": nanoid()}, SECRET_KEY, algorithm=ALGORITHM)
---
+++ backend/app/utils.py
@@ -0,0 +1,4 @@
+import nanoid as nanoid_package
+
+def nanoid() -> str:
+    return nanoid_package.generate(alphabet="0123456789abcdefghijklmnopqrstuvwxyz", size=12)
