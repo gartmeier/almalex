5b9eb61240d7e78efb5b6d80e90b52ec0484ec58
Joshua Gartmeier <joshua@gartmeier.dev>
2025-04-24 20:15:58 +0200

    feat(backend): add create token endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 import jwt
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from app.conf import settings
 from app.utils import nanoid
@@ -7,8 +8,22 @@ from app.utils import nanoid
 SECRET_KEY = settings.secret_key
 ALGORITHM = "HS256"
 
+
+class Token(BaseModel):
+    access_token: str
+
+
 app = FastAPI()
 
-@app.post("/token")
-async def login_for_access_token():
-    return jwt.encode({"sub": nanoid()}, SECRET_KEY, algorithm=ALGORITHM)
+
+@app.post("/token", response_model=Token)
+async def create_token():
+    access_token = jwt.encode(
+        {
+            "sub": nanoid(),
+            "type": "anonymous",
+        },
+        SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+    return Token(access_token=access_token)
--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,7 @@
 import nanoid as nanoid_package
 
+
 def nanoid() -> str:
-    return nanoid_package.generate(alphabet="0123456789abcdefghijklmnopqrstuvwxyz", size=12)
+    return nanoid_package.generate(
+        alphabet="0123456789abcdefghijklmnopqrstuvwxyz", size=12
+    )
