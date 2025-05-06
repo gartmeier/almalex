bbcb194664724bc3829ed53fb859b92a4488d984
Joshua Gartmeier <joshua@gartmeier.dev>
2025-05-06 21:03:33 +0200

    refactor: streamline server-sent event parsing logic

--- frontend/app/utils.ts
+++ frontend/app/utils.ts
@@ -6,33 +6,30 @@ export const nanoid = customAlphabet(
 );
 
 export function parseServerSentEvents(rawEvents: string) {
-  return rawEvents
-    .trim()
-    .split("\n\n")
-    .map(parseServerSentEvent)
-    .filter((a) => !!a);
-}
+  let parsedEvents: { name: string; data: string }[] = [];
 
-function parseServerSentEvent(rawEvent: string) {
-  let lines = rawEvent.split("\n");
+  for (let rawEvent of rawEvents.split("\n\n")) {
+    if (rawEvent) {
+      let lines = rawEvent.split("\n");
 
-  let name: string | null = null;
-  let data: string | null = null;
+      let name: string | null = null;
+      let data: string | null = null;
 
-  for (let line of lines) {
-    if (line.startsWith("event: ")) {
-      name = line.slice(7);
-    } else if (line.startsWith("data: ")) {
-      data = line.slice(6);
+      for (let line of lines) {
+        if (line.startsWith("event: ")) {
+          name = line.slice(7);
+        } else if (line.startsWith("data: ")) {
+          data = line.slice(6);
+        }
+      }
+
+      if (!name || !data) {
+        console.error("Invalid server-sent event", rawEvent);
+        continue;
+      }
+
+      parsedEvents.push({ name, data });
     }
   }
-
-  console.log(name, data);
-
-  if (!name || !data) {
-    console.error("Invalid server-sent event", rawEvent);
-    return null;
-  }
-
-  return { name, data };
+  return parsedEvents;
 }
