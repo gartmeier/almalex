dfe4e7ab43056f40b5ed83ed296c8524bc88e8a6
Joshua Gartmeier <joshua@gartmeier.dev>
2025-05-17 20:13:59 +0200

    feat(cli): add prompt command to handle user questions

--- backend/cli/main.py
+++ backend/cli/main.py
@@ -2,6 +2,7 @@ import click
 
 from cli.cmd.load_bge import load_bge
 from cli.cmd.load_fedlex import load_fedlex
+from cli.cmd.prompt import prompt
 from cli.cmd.search import search
 
 
@@ -12,6 +13,7 @@ def main():
 
 main.add_command(load_bge)
 main.add_command(load_fedlex)
+main.add_command(prompt)
 main.add_command(search)
 
 if __name__ == "__main__":
