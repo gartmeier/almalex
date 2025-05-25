cebe5fbd9c6937d79043582b160ed26185232a29
Joshua Gartmeier <joshua@gartmeier.dev>
2025-05-25 12:01:02 +0200

    feat(backend): add Jinja2 for template rendering

---
+++ backend/app/utils/template.py
@@ -0,0 +1,13 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader("app"),
+    autoescape=select_autoescape(),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
+
+
+def render(template_name: str, **kwargs) -> str:
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
