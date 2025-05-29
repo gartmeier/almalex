8db6f9cda53a850f29bca78514f8af3ee007d187
Joshua Gartmeier <joshua@gartmeier.dev>
2025-05-29 02:38:08 +0200

    style(frontend): adjust chat container padding and scrollbar

--- frontend/app/chat/messages.tsx
+++ frontend/app/chat/messages.tsx
@@ -4,8 +4,8 @@ import { Message } from "~/chat/message";
 export function Messages() {
   const { messages } = useChatContext();
   return (
-    <div className="flex-1 overflow-y-auto">
-      <div className="mx-auto max-w-3xl px-5">
+    <div className="flex-1 overflow-y-auto" style={{scrollbarGutter: "stable both-edges"}}>
+      <div className="mx-auto max-w-3xl px-3">
         {messages.map((message) => (
           <Message key={message.id} message={message} />
         ))}
