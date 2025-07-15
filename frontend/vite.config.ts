1205d4fa330f2834b5b0f6f00b228eda29fd8444
Joshua Gartmeier <joshua@gartmeier.dev>
2025-07-15 13:01:05 +0200

    chore: configure sentry sourcemaps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), sentryVitePlugin({
+    org: "gartmeier",
+    project: "almalex_frontend"
+  })],
+
   server: {
     proxy: {
       "/api": "http://localhost:8000",
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
