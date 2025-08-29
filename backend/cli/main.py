ced3e0f2d39bfa215d8f44400854830d6bbe456f
Joshua Gartmeier <joshua@gartmeier.dev>
2025-08-29 21:16:19 +0200

    fix: fedlex urls with a command

--- backend/cli/main.py
+++ backend/cli/main.py
@@ -1,9 +1,8 @@
 import click
 
+from cli.cmd.fix_fedlex_urls import fix_fedlex_urls
 from cli.cmd.load_bge import load_bge
 from cli.cmd.load_fedlex import load_fedlex
-from cli.cmd.prompt import prompt
-from cli.cmd.search import search
 from cli.cmd.shell import shell
 
 
@@ -12,10 +11,9 @@ def main():
     pass
 
 
+main.add_command(fix_fedlex_urls)
 main.add_command(load_bge)
 main.add_command(load_fedlex)
-main.add_command(prompt)
-main.add_command(search)
 main.add_command(shell)
 
 if __name__ == "__main__":
