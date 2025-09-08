f9df10796769da29889a273f074994e741c67045
Joshua Gartmeier <joshua@gartmeier.dev>
2025-09-08 08:06:15 +0200

    feat: add message input

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,3 +1,15 @@
+import { MessageInput } from "~/components/message-input";
+
 export default function Component() {
-  return <div>Chat</div>;
+  function handleSubmit() {}
+
+  return (
+    <div>
+      <div className="absolute bottom-4 w-full px-4">
+        <div className="mx-auto max-w-3xl">
+          <MessageInput onSubmit={handleSubmit} />
+        </div>
+      </div>
+    </div>
+  );
 }
